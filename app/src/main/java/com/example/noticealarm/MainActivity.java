282b00d5e2b0ea5a7b31ab9e3426f618476316ff
JAESEONGLEE <lee01042000@gmail.com>
2020-09-21 23:16:01 +0900

    create NoticeCrawling.java.gettableElements() methods

--- app/src/main/java/com/example/noticealarm/MainActivity.java
+++ app/src/main/java/com/example/noticealarm/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import org.jsoup.select.Elements;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        NoticeCrawling noticeCrawling=new NoticeCrawling("http://computer.knu.ac.kr/06_sub/02_sub.html");
+        Elements elements=noticeCrawling.getTableElements();
     }
 }
