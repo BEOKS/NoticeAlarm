42092354270e3ea2a9e0168e72e5c04889b45cd9
BEOKS <lee01042000@gmail.com>
2020-09-27 18:45:00 +0900

    delete NoticeCrawling.java

--- app/src/main/java/com/example/noticealarm/MainActivity.java
+++ app/src/main/java/com/example/noticealarm/MainActivity.java
@@ -12,7 +12,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        NoticeCrawling noticeCrawling=new NoticeCrawling("http://computer.knu.ac.kr/06_sub/02_sub.html");
-        Elements elements=noticeCrawling.getTableElements();
     }
 }
