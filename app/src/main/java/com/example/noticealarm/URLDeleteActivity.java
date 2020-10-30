f5154a806a744f55c3e305067b7121c915279043
BEOKS <lee01042000@gmail.com>
2020-10-30 22:52:43 +0900

    이건 제가 할게요!는 제가 할게요 ㅎㅎ

--- app/src/main/java/com/example/noticealarm/URLDeleteActivity.java
+++ app/src/main/java/com/example/noticealarm/URLDeleteActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 /**
+ * 이건 제가 할게요!
  * 1. intent로 넘겨받은 카테고리 이름을 통해서
  *      ArrayList<Data>  getURLinCategory(String categoryName)를 호출하여 데이터 리스트를 입력받음
  *      각 데이터의 urlName을 textView로 하고 삭제 버튼을 결합한 뷰를 생성하여 레이아웃에 추가함
